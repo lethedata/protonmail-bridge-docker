5621ad01aa9518068a1ec012a876dc7b6817dd4f
Echo Nar <echo@lethedata.com>
2026-03-11 14:19:29 -0500

    feat: disable telemetry and autoupdates by default
    Patch default settings in vault types_settings

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,7 @@ VERSION=v`cat VERSION`
 # Clone new code
 git clone --depth 1 --branch $VERSION https://github.com/ProtonMail/proton-bridge.git
 cd proton-bridge
+git apply ../default-vault-types_settings.patch
 
 # Build
 make build-nogui vault-editor
