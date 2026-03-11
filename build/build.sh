ab5aba947675c172007b99db40f93cd85de0b158
Echo Nar <echo@lethedata.com>
2026-03-11 14:16:50 -0500

    chore: shallow clone tag
    Don't need to clone entire repo, just the release tag

--- build/build.sh
+++ build/build.sh
@@ -2,12 +2,11 @@
 
 set -ex
 
-VERSION=`cat VERSION`
+VERSION=v`cat VERSION`
 
 # Clone new code
-git clone https://github.com/ProtonMail/proton-bridge.git
+git clone --depth 1 --branch $VERSION https://github.com/ProtonMail/proton-bridge.git
 cd proton-bridge
-git checkout v$VERSION
 
 # Build
 make build-nogui vault-editor
